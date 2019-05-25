e0103c1a119a628722fe124ba63faad4b2c75ec3
Anuja <avanivyavahare@gmail.com>
2019-05-25 10:44:09 +0530

    first commit

---
+++ src/main/java/com/qa/utils/CustomizedListner.java
@@ -0,0 +1,61 @@
+package com.qa.utils;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGListener;
+import org.testng.ITestResult;
+
+public class CustomizedListner extends TestUtils implements ITestListener{
+
+	@Override
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+	
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Failed Testcases.....");
+		TestUtils utils=new TestUtils();
+		try {
+		utils.failed(result.getMethod().getMethodName());
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	} 
+
+}
